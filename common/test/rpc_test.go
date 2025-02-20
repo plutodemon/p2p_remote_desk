0767d99418465598d81df1fead1fc352248f3b2c
lemon <101779730+plutodemon@users.noreply.github.com>
2025-02-20 19:08:19 +0800

    update: rpc

---
+++ common/test/rpc_test.go
@@ -0,0 +1,60 @@
+package test
+
+import (
+	"log"
+	"net"
+	"net/rpc"
+	"net/rpc/jsonrpc"
+	"testing"
+)
+
+type HelloServiceInterface interface {
+	Hello(request string, reply *string) error
+}
+
+type HelloService struct{}
+
+func (p *HelloService) Hello(request *String, reply *string) error {
+	*reply = "hello:" + request.GetValue()
+	return nil
+}
+
+const HelloServiceName = "HelloService"
+
+func RegisterHelloService(svc HelloServiceInterface) error {
+	return rpc.RegisterName(HelloServiceName, svc)
+}
+
+func Test_RpcServer(t *testing.T) {
+	err := RegisterHelloService(new(HelloService))
+	if err != nil {
+		return
+	}
+
+	listener, err := net.Listen("tcp", ":1234")
+	if err != nil {
+		log.Fatal("ListenTCP error:", err)
+	}
+
+	conn, err := listener.Accept()
+	if err != nil {
+		log.Fatal("Accept error:", err)
+	}
+
+	rpc.ServeCodec(jsonrpc.NewServerCodec(conn))
+}
+
+func Test_RpcClient(t *testing.T) {
+	client, err := rpc.Dial("tcp", "localhost:1234")
+	if err != nil {
+		log.Fatal("Dialing:", err)
+	}
+
+	// client := rpc.NewClient(conn)
+	var reply string
+	err = client.Call("HelloService.Hello", "hello", &reply)
+	if err != nil {
+		log.Fatal("Call error:", err)
+	}
+	log.Println(reply)
+}
--- go.mod
+++ go.mod
@@ -15,6 +15,8 @@ require (
 	github.com/pion/webrtc/v4 v4.0.9
 	github.com/plutodemon/llog v0.1.0
 	github.com/stretchr/testify v1.10.0
+	google.golang.org/grpc v1.38.0
+	google.golang.org/protobuf v1.34.1
 )
 
 require (
@@ -41,6 +43,7 @@ require (
 	github.com/go-text/typesetting v0.2.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/godbus/dbus/v5 v5.1.0 // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/gopherjs/gopherjs v1.17.2 // indirect
 	github.com/jeandeaual/go-locale v0.0.0-20240223122105-ce5225dcaa49 // indirect
@@ -85,7 +88,7 @@ require (
 	golang.org/x/net v0.34.0 // indirect
 	golang.org/x/sys v0.29.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
-	google.golang.org/protobuf v1.34.1 // indirect
+	google.golang.org/genproto v0.0.0-20210602131652-f16073e35f0c // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
